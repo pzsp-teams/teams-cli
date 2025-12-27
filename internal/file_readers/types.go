972a5d500e279d75461b3c5af03efbdc96b62c75
KamilMarszalek <kamilmar553@gmail.com>
2025-12-27 21:57:45 +0100

    feat: implement decoding functions for CSV formats

--- internal/file_readers/types.go
+++ internal/file_readers/types.go
@@ -1,12 +1,7 @@
 package file_readers
 
 import (
-	"encoding/json"
-	"fmt"
 	"io"
-
-	"github.com/BurntSushi/toml"
-	"gopkg.in/yaml.v3"
 )
 
 // TemplateData represents placeholder values for a single message recipient
@@ -20,31 +15,10 @@ const (
 	JSON Formats = "json"
 	YAML Formats = "yaml"
 	TOML Formats = "toml"
+	CSV  Formats = "csv"
 )
 
 // DecodeFunc defines a function type for decoding data from an io.Reader into a provided structure
 type DecodeFunc func(r io.Reader, v any) error
 
-// DecodeJSON decodes JSON data from the reader into the provided structure
-func DecodeJSON(r io.Reader, v any) error {
-	if err := json.NewDecoder(r).Decode(v); err != nil {
-		return fmt.Errorf(decodeFailedTemplate, JSON, err)
-	}
-	return nil
-}
 
-// DecodeYAML decodes YAML data from the reader into the provided structure
-func DecodeYAML(r io.Reader, v any) error {
-	if err := yaml.NewDecoder(r).Decode(v); err != nil {
-		return fmt.Errorf(decodeFailedTemplate, YAML, err)
-	}
-	return nil
-}
-
-// DecodeTOML decodes TOML data from the reader into the provided structure
-func DecodeTOML(r io.Reader, v any) error {
-	if _, err := toml.NewDecoder(r).Decode(v); err != nil {
-		return fmt.Errorf(decodeFailedTemplate, TOML, err)
-	}
-	return nil
-}
