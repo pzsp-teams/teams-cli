26c286f2219270da4ae1d44aeb84e9fe8fcd40e7
KamilMarszalek <kamilmar553@gmail.com>
2025-12-28 10:41:57 +0100

    feat: add GroupBy function for grouping items by a key

--- internal/file_readers/decode.go
+++ internal/file_readers/decode.go
@@ -104,3 +104,14 @@ func (d *CSVDecoder) Decode(v any) error {
 	*dst = record
 	return nil
 }
+
+func GroupBy[T any, K comparable](items []T, keyFunc func(T) K) map[K][]T {
+	result := make(map[K][]T, len(items))
+	for _, item := range items {
+		key := keyFunc(item)
+		values := result[key]
+		values = append(values, item)
+		result[key] = values
+	}
+	return result
+}
