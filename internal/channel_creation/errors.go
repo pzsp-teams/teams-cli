88bdf3d4c850e9339d21fc04c2d34f598af1c0d4
KamilMarszalek <kamilmar553@gmail.com>
2025-12-29 16:54:11 +0100

    fix: formatting and linter issues

--- internal/channel_creation/errors.go
+++ internal/channel_creation/errors.go
@@ -3,6 +3,6 @@ package channelcreation
 import "errors"
 
 var (
-	errDataParseFailed = errors.New("failed to parse channels data")
+	errDataParseFailed         = errors.New("failed to parse channels data")
 	errMembersPartiallyEnsured = errors.New("some members or owners could not be ensured in the channel")
 )
