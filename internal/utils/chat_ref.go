c8965fd26b967293768419b7dbcde7bb642617ca
Damian D'Souza <dsouzadamian189@gmail.com>
2025-12-31 21:04:33 +0100

    feat: implement helper functions to get ChatRef

---
+++ internal/utils/chat_ref.go
@@ -0,0 +1,14 @@
+package utils
+
+import (
+	"github.com/pzsp-teams/lib/chats"
+)
+
+// GetChatRef returns a ChatRef for either group chat or
+// one on one chat based on if given ref is an email
+func GetChatRef(ref string) chats.ChatRef {
+	if IsLikelyEmail(ref) {
+		return chats.OneOnOneChatRef{Ref: ref}
+	}
+	return chats.GroupChatRef{Ref: ref}
+}
