85876e02bb4eb71aa4ff2ba0320353b42c4c7dd1
Damian D'Souza <dsouzadamian189@gmail.com>
2026-01-01 23:49:24 +0100

    style: add comments to exported noopLogger functions

--- internal/initializers/testing.go
+++ internal/initializers/testing.go
@@ -6,14 +6,24 @@ import "github.com/pzsp-teams/cli/internal/logger"
 
 type noopLogger struct{}
 
+// With returns a new logger with additional context fields. For noopLogger, this is a no-op.
 func (noopLogger) With(args ...any) logger.Logger { return noopLogger{} }
-func (noopLogger) Debug(msg string, args ...any)  {}
-func (noopLogger) Info(msg string, args ...any)   {}
-func (noopLogger) Warn(msg string, args ...any)   {}
-func (noopLogger) Error(msg string, args ...any)  {}
+
+// Debug logs a debug message. For noopLogger, this is a no-op.
+func (noopLogger) Debug(msg string, args ...any) {}
+
+// Info logs an info message. For noopLogger, this is a no-op.
+func (noopLogger) Info(msg string, args ...any) {}
+
+// Warn logs a warning message. For noopLogger, this is a no-op.
+func (noopLogger) Warn(msg string, args ...any) {}
+
+// Error logs an error message. For noopLogger, this is a no-op.
+func (noopLogger) Error(msg string, args ...any) {}
 
 var _ logger.Logger = noopLogger{}
 
+// SetupTestLogger configures the global Logger to use a no-op implementation for testing.
 func SetupTestLogger() {
 	Logger = noopLogger{}
 }
