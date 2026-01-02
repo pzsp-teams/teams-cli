561d680e878d133b9324c5c73dfaaafbf95de263
Damian D'Souza <dsouzadamian189@gmail.com>
2026-01-02 17:25:27 +0100

    chore: remove unused chat and channel types

--- internal/messaging/channel_types.go
+++ internal/messaging/channel_types.go
@@ -1,26 +1,8 @@
 package messaging
 
-import (
-	"context"
-
-	"github.com/pzsp-teams/lib/models"
-)
-
 // ChannelSendResult represents the result of sending a message to a channel
 type ChannelSendResult struct {
 	ChannelRef string
 	Message    string
 	Error      error
-}
-
-type channelMessageData struct {
-	teamRef    string
-	channelRef string
-	body       models.MessageBody
-}
-
-type channelAction struct {
-	*channelMessageData
-	run    func(ctx context.Context, message *channelMessageData) *ChannelSendResult
-	result *ChannelSendResult
 }
--- internal/messaging/chat_types.go
+++ internal/messaging/chat_types.go
@@ -1,25 +1,8 @@
 package messaging
 
-import (
-	"context"
-
-	"github.com/pzsp-teams/lib/models"
-)
-
 // ChatSendResult represents the result of sending a message to a chat
 type ChatSendResult struct {
 	ChatRef string
 	Message string
 	Error   error
-}
-
-type chatMessageData struct {
-	chatRef string
-	body    models.MessageBody
-}
-
-type chatAction struct {
-	*chatMessageData
-	run    func(ctx context.Context, message *chatMessageData) *ChatSendResult
-	result *ChatSendResult
 }
