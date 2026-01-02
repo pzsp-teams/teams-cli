a1e3a48e79452fc824d778367226fe27db7acef2
Damian D'Souza <dsouzadamian189@gmail.com>
2026-01-02 17:19:49 +0100

    feat: implement generic sender and sender adapter

---
+++ internal/messaging/sender_adapter.go
@@ -0,0 +1,29 @@
+package messaging
+
+import (
+	"context"
+
+	"github.com/pzsp-teams/lib/models"
+)
+
+// senderAdapter abstracts type-specific operations for channel and chat senders.
+// Res: Result type
+type senderAdapter[Res any] interface {
+	getMentions(ctx context.Context, ref string, rawMentions []string) ([]models.Mention, error)
+	sendMessage(ctx context.Context, ref string, body models.MessageBody) (*models.Message, error)
+	newErrorResult(ref string, content string, err error) Res
+	newSuccessResult(ref string, message string) Res
+	getError(result Res) error
+	getLogFields(ref string) map[string]any
+}
+
+type messageData struct {
+	ref  string
+	body models.MessageBody
+}
+
+type action[Res any] struct {
+	data   *messageData
+	run    func(ctx context.Context, data *messageData) Res
+	result *Res
+}
