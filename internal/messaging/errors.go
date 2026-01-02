4d501a61d596471f7a4639147e8551ede5939d92
Damian D'Souza <dsouzadamian189@gmail.com>
2026-01-02 17:20:25 +0100

    feat: add errors for failed send and mention resolution

--- internal/messaging/errors.go
+++ internal/messaging/errors.go
@@ -2,5 +2,13 @@ package messaging
 
 import "errors"
 
-// ErrMessageSkipped marks a message as skipped in case ignoreError is not set when sending messages
-var ErrMessageSkipped = errors.New("skipped sending message, earlier message failed")
+var (
+	// ErrMessageSkipped marks a message as skipped in case ignoreError is not set when sending messages
+	ErrMessageSkipped = errors.New("skipped sending message, earlier message failed")
+
+	// ErrMentionResolutionFailed indicates that mention resolution failed
+	ErrMentionResolutionFailed = errors.New("failed to resolve mentions")
+
+	// ErrMessageSendFailed indicates that sending a message failed
+	ErrMessageSendFailed = errors.New("failed to send message")
+)
