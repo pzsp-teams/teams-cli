4ae5c9be674b6f68bd7a438dba379eee51fc53ab
Damian D'Souza <dsouzadamian189@gmail.com>
2026-01-03 23:56:13 +0100

    chore: remove old files

--- internal/channels/retriever/generate.go
+++
@@ -1,4 +0,0 @@
-package retriever
-
-//go:generate mockgen -destination=mock_teams_service_test.go -package=retriever -mock_names=Service=mockTeamsService github.com/pzsp-teams/lib/teams Service
-//go:generate mockgen -destination=mock_channels_service_test.go -package=retriever -mock_names=Service=mockChannelsService github.com/pzsp-teams/lib/channels Service
