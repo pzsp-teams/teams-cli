91a328122ee2811b74816a4ed8fd144e53dba82e
Damian D'Souza <dsouzadamian189@gmail.com>
2026-01-10 08:36:05 +0100

    refactor: log channel creation in wrapper getter

--- cmd/common/client.go
+++ cmd/common/client.go
@@ -4,10 +4,19 @@ import (
 	"github.com/spf13/cobra"
 
 	"github.com/pzsp-teams/cli/internal/client"
+	"github.com/pzsp-teams/cli/internal/initializers"
 )
 
 // GetTeamsClient returns the TeamsClient instance using the command's context
 // This is a wrapper around client.GetOrCreateInstance for use in cmd layer
 func GetTeamsClient(cmd *cobra.Command) (*client.TeamsClient, error) {
-	return client.GetOrCreateInstance(cmd.Context())
+	log := initializers.Logger
+	log.Debug("Creating Teams client")
+
+	teamsClient, err := client.GetOrCreateInstance(cmd.Context())
+	if err != nil {
+		log.Error("Failed to create Teams client", "error", err)
+	}
+
+	return teamsClient, err
 }
