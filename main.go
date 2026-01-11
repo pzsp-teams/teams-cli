70b7aadb092bcc71756f8bd13041f18b4f808732
KamilMarszalek <kamilmar553@gmail.com>
2026-01-11 23:41:35 +0100

    feat: add initialization for password hashing pepper

--- main.go
+++ main.go
@@ -1,9 +1,21 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/pzsp-teams/cli/cmd"
 	"github.com/pzsp-teams/lib"
+	"github.com/pzsp-teams/lib/setup"
 )
+
+func init() {
+	if !setup.PepperExists() {
+		var pepper string
+		print("Set a pepper for hashing passwords: ")
+		fmt.Scanln(&pepper)
+		setup.SetPepper(pepper)
+	}
+}
 
 func main() {
 	defer lib.Close()
