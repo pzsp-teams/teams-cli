4585f5edb8b967aee26e436c8fec2c9c6cd110bd
KamilMarszalek <kamilmar553@gmail.com>
2026-01-11 23:48:10 +0100

    feat: improve error handling for password pepper

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/pzsp-teams/cli/cmd"
 	"github.com/pzsp-teams/lib"
@@ -12,8 +13,16 @@ func init() {
 	if !setup.PepperExists() {
 		var pepper string
 		print("Set a pepper for hashing passwords: ")
-		fmt.Scanln(&pepper)
-		setup.SetPepper(pepper)
+		_, err := fmt.Scanln(&pepper)
+		if err != nil {
+			fmt.Println("Error reading pepper:", err)
+			os.Exit(1)
+		}
+		err = setup.SetPepper(pepper)
+		if err != nil {
+			fmt.Println("Error setting pepper:", err)
+			os.Exit(1)
+		}
 	}
 }
 
