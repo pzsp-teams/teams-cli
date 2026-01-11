43fb6c5cf144f3995f773492fd67f8829259c5fa
Damian D'Souza <dsouzadamian189@gmail.com>
2026-01-11 03:43:37 +0100

    feat: implement navigation helpers

---
+++ tui/navigation.go
@@ -0,0 +1,36 @@
+package tui
+
+import (
+	"github.com/pzsp-teams/cli/app"
+)
+
+// navigationState tracks current command and navigation history
+type navigationState struct {
+	current *app.CommandDef
+	history []*app.CommandDef
+}
+
+func newNavigationState() *navigationState {
+	return &navigationState{
+		current: nil, // Root
+		history: []*app.CommandDef{},
+	}
+}
+
+func (n *navigationState) navigateTo(def *app.CommandDef) {
+	n.history = append(n.history, n.current)
+	n.current = def
+}
+
+func (n *navigationState) goBack() bool {
+	if len(n.history) == 0 {
+		return false
+	}
+	n.current = n.history[len(n.history)-1]
+	n.history = n.history[:len(n.history)-1]
+	return true
+}
+
+func (n *navigationState) getCurrentCommand() *app.CommandDef {
+	return n.current
+}
