fdac12d87eeba8ba0ab5e3bd3aa7fd65e029f5b8
KamilMarszalek <kamilmar553@gmail.com>
2026-01-12 20:10:02 +0100

    refactor: optimize GetChatRef by trimming whitespace earlier

--- internal/utils/chat_ref.go
+++ internal/utils/chat_ref.go
@@ -9,11 +9,12 @@ import (
 // GetChatRef returns a ChatRef for either group chat or
 // one on one chat based on if given ref is an email
 func GetChatRef(ref string) chats.ChatRef {
+	ref = strings.TrimSpace(ref)
+	if ref == "" {
+		return nil
+	}
 	if IsLikelyEmail(ref) || IsLikelyOneOnOneChatID(ref) {
 		return chats.OneOnOneChatRef{Ref: ref}
-	}
-	if strings.TrimSpace(ref) == "" {
-		return nil
 	}
 	return chats.GroupChatRef{Ref: ref}
 }
