a87dc545269b24211359528576e0c03b5bd4ed44
KamilMarszalek <kamilmar553@gmail.com>
2026-01-12 15:02:46 +0100

    feat: enhance GetChatRef to support one-on-one chat ID detection

--- internal/utils/chat_ref.go
+++ internal/utils/chat_ref.go
@@ -7,7 +7,7 @@ import (
 // GetChatRef returns a ChatRef for either group chat or
 // one on one chat based on if given ref is an email
 func GetChatRef(ref string) chats.ChatRef {
-	if IsLikelyEmail(ref) {
+	if IsLikelyEmail(ref) || IsLikelyOneOnOneChatID(ref) {
 		return chats.OneOnOneChatRef{Ref: ref}
 	}
 	return chats.GroupChatRef{Ref: ref}
